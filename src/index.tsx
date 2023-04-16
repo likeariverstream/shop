bbf2a1e83ca05751346298b4d7d6be169c8b6ec0
Sergey Kalachev <rollstream@yandex.ru>
2023-04-16 13:46:58 +0700

    router fixed

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app/App'
 import './styles/index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <BrowserRouter>
+    <Router>
         <React.StrictMode>
             <App />
         </React.StrictMode>,
-    </BrowserRouter>
+    </Router>
 )
